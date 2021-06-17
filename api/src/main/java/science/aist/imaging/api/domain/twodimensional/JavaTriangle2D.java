68af1582f32ac32e0f128848abce6b6183c24032
Christoph Praschl <christoph.praschl@fh-hagenberg.at>
2021-06-17 08:05:05 +0200

    #25 Added missing copyright headers

--- api/src/main/java/science/aist/imaging/api/domain/twodimensional/JavaTriangle2D.java
+++ api/src/main/java/science/aist/imaging/api/domain/twodimensional/JavaTriangle2D.java
@@ -1,6 +1,10 @@
 /*
- * Copyright (c) Johannes Diemke and contributors. All rights reserved.
- * Licensed under the MIT license. See LICENSE_MIT_JDIEMKE file in the project root for details.
+ * Copyright (c) 2021 the original author or authors.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 
 package science.aist.imaging.api.domain.twodimensional;
